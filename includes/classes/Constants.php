709c33c8299ec483b92e8075bac7d2bdd8f90149
Pedro Sousa <jpedroleitedesousa@gmail.com>
2023-03-14 18:35:59 -0300

    Constants Class

---
+++ includes/classes/Constants.php
@@ -0,0 +1,18 @@
+<?php
+class Constants {
+
+	public static $passwordsDoNoMatch = "As senhas informadas não são iguais";
+	public static $passwordNotAlphanumeric = "Por favor, informe na sua senha apenas caracteres alfanuméricos";
+	public static $passwordCharacters = "Sua senha deve ter entre 5 e 30 caracteres";
+	public static $emailInvalid = "E-mail não válido";
+	public static $emailsDoNotMatch =  "Os e-mails informadas não são iguais";
+	public static $emailTaken = "E-mail já esta sendo usado por outra conta";
+	public static $lastNameCharacters = "Seu ultimo nome deve conter entre 2 até 25 caracteres";
+	public static $firstNameCharacters = "Seu primeiro nome deve conter entre 2 até 25 caracteres";
+	public static $usernameCharacters = "Seu nome de usuário deve conteer entre 2 até 25 caracteres";
+	public static $usernameTaken = "Esse nome de usuário já está e uso por outra conta";
+
+	public static $loginFailed = "Seu e-mail e/ou senha estão incorretos";
+
+}
+?>
